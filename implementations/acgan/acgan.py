1162bcd101d91a530468558dfc8282eac4452811
sglidq625 <sglidq625@gmail.com>
2018-05-01 00:14:04 +0800

    Update format

--- implementations/acgan/acgan.py
+++ implementations/acgan/acgan.py
@@ -91,32 +91,45 @@ def sample_image(n_row, epoches_done, saved_img_dir="images"):
     # Sample noise
     z = torch.randn((n_row ** 2, opt.latent_dim)).to(device)
     # Get labels ranging from 0 to n_classes for n rows
-    labels = torch.stack([torch.arange(end=n_row) for _ in range(n_row)]).reshape((n_row ** 2)).long().to(device)
+    labels = torch.stack([torch.arange(end=n_row) for _ in range(n_row)])
+    labels = labels.reshape((n_row ** 2)).long().to(device)
     gen_imgs = generator(z, labels)
-    save_image(gen_imgs, "{}/{}.png".format(saved_img_dir, epoches_done), nrow=n_row, normalize=True)
+    save_image(gen_imgs, "{}/{}.png".format(saved_img_dir, epoches_done), 
+               nrow=n_row, normalize=True)
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--n_epochs', type=int, default=50, help='number of epochs of training (default: 50)')
-    parser.add_argument('--batch_size', type=int, default=64, help='size of the batches (default: 64)')
-    parser.add_argument('--lr', type=float, default=0.0002, help='adam: learning rate (default: 0.0002)')
+    parser.add_argument('--n_epochs', type=int, default=50, 
+                        help='number of epochs of training (default: 50)')
+    parser.add_argument('--batch_size', type=int, default=64, 
+                        help='size of the batches (default: 64)')
+    parser.add_argument('--lr', type=float, default=0.0002, 
+                        help='adam: learning rate (default: 0.0002)')
     parser.add_argument('--b1', type=float, default=0.5,
                         help='adam: coefficient in computing running averages of gradient (default: 0.5)')
     parser.add_argument('--b2', type=float, default=0.999,
                         help='adam: coefficient in computing running averages of gradient\'s square (default: 0.999)')
-    parser.add_argument('--latent_dim', type=int, default=100, help='dimensionality of the latent space (default: 100)')
-    parser.add_argument('--n_classes', type=int, default=10, help='number of classes for dataset (default: 10)')
-    parser.add_argument('--img_size', type=int, default=32, help='size of each image dimension (default: 32)')
-    parser.add_argument('--channels', type=int, default=1, help='number of image channels (default: 1)')
-    parser.add_argument('--saved_img_dir', type=str, default='images', help='dir of saved images (default: images)')
+    parser.add_argument('--latent_dim', type=int, default=100, 
+                        help='dimensionality of latent space (default: 100)')
+    parser.add_argument('--n_classes', type=int, default=10, 
+                        help='number of classes for dataset (default: 10)')
+    parser.add_argument('--img_size', type=int, default=32, 
+                        help='size of each image dimension (default: 32)')
+    parser.add_argument('--channels', type=int, default=1, 
+                        help='number of image channels (default: 1)')
+    parser.add_argument('--saved_img_dir', type=str, default='images', 
+                        help='dir of saved images (default: images)')
     parser.add_argument('--saved_models', type=str, default='save_models',
                         help='dir of saved images (default: save_models)')
     parser.add_argument('--n_cpu', type=int, default=8,
                         help='number of cpu threads to use during batch generation (default: 8)')
-    parser.add_argument('--disable_gpu', action='store_true', help='Flag whether to disable GPU')
-    parser.add_argument('--enable_tensorboard', action='store_false', help='Flag whether to enable TensorBoard')
-    parser.add_argument('--multi_gpu', action='store_true', help='Flag whether to use multiple GPUs.')
+    parser.add_argument('--disable_gpu', action='store_true', 
+                        help='Flag whether to disable GPU')
+    parser.add_argument('--enable_tensorboard', action='store_false', 
+                        help='Flag whether to enable TensorBoard')
+    parser.add_argument('--multi_gpu', action='store_true', 
+                        help='Flag whether to use multiple GPUs.')
 
     opt = parser.parse_args()
     print(opt)
@@ -129,7 +142,8 @@ if __name__ == '__main__':
         logger = Logger('logs')
 
     # Initialize generator and discriminator
-    device = torch.device("cuda" if not opt.disable_gpu and torch.cuda.is_available() else "cpu")
+    device = torch.device("cuda" if not opt.disable_gpu and 
+                          torch.cuda.is_available() else "cpu")
     generator = Generator().to(device)
     discriminator = Discriminator().to(device)
     if opt.multi_gpu and torch.cuda.device_count() > 1:
@@ -154,8 +168,12 @@ if __name__ == '__main__':
     dataloader = torch.utils.data.DataLoader(trainset, batch_size=opt.batch_size, shuffle=True, num_workers=opt.n_cpu)
 
     # Optimizers
-    optimizer_G = torch.optim.Adam(generator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
-    optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
+    optimizer_G = torch.optim.Adam(generator.parameters(), 
+                                   lr=opt.lr, 
+                                   betas=(opt.b1, opt.b2))
+    optimizer_D = torch.optim.Adam(discriminator.parameters(), 
+                                   lr=opt.lr, 
+                                   betas=(opt.b1, opt.b2))
 
     # ----------
     #  Training
@@ -163,7 +181,8 @@ if __name__ == '__main__':
 
     for epoch in range(opt.n_epochs):
 
-        sample_image(n_row=10, epoches_done=epoch, saved_img_dir=opt.saved_img_dir)
+        sample_image(n_row=10, epoches_done=epoch, 
+                     saved_img_dir=opt.saved_img_dir)
 
         for i, (imgs, labels) in enumerate(dataloader):
 
@@ -226,8 +245,8 @@ if __name__ == '__main__':
             logger.scalar_summary("D acc: ", 100 * d_acc, epoch)  #
         else:
             print ("[Epoch {}/{}] [Batch {}/{}] [D loss: {}, acc: {}%%] [G loss: {}]".format(epoch, opt.n_epochs,
-                                                                                             i, len(dataloader),
-                                                                                             d_loss.item(), 100 * d_acc,
-                                                                                             g_loss.item()))
+                                  i, len(dataloader),
+                                  d_loss.item(), 100 * d_acc,
+                                  g_loss.item()))
 
         torch.save(generator.state_dict(), "{}/generator.th".format(opt.saved_models))
